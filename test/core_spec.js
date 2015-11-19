ac3282b3afec0256cf6fdffd7449f20650cc3b1e
Anurag Ramdasan <anuragr@live.com>
2015-11-19 22:54:10 +0530

    complete core logic for next

--- test/core_spec.js
+++ test/core_spec.js
@@ -15,6 +15,15 @@ describe('application logic', () => {
       expect(nextState).to.
         equal(Map({'entries': List.of('Trainspotting', '28 Days Later')}));
     });
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+
+      expect(nextState).to.
+        equal(Map({'entries': List.of('Trainspotting', '28 Days Later')}));
+    });
   });
 
   describe('next', () => {
@@ -31,6 +40,66 @@ describe('application logic', () => {
           pair: List.of('Trainspotting', '28 Days Later')
         }),
         entries: List.of('Sunshine')
+      }));
+    });
+
+    it('puts winner of current vote back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting')
+      }));
+    });
+
+    it('puts both from tied back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 2,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+      }));
+    });
+
+    it('marks winner when just one entry left', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', 'Sunshine'),
+          tally: Map({
+            'Trainspotting': 4,
+            'Sunshine': 1
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+      console.log(nextState);
+      expect(nextState).to.equal(Map({
+        'winner': 'Trainspotting'
       }));
     });
   });
